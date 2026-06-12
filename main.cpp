025164972902df8072300017b26350bec504eceb
Marysiat <taurus7809@gmail.com>
2026-06-12 12:36:59 +0200

    Wstawianie statków przez komputer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <random>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 int main() {
-string a;
+string a, odpowiedz;
 int b;
 string table[10][10]=
 {
@@ -51,120 +52,29 @@ for(int k=0; k<20; k++)
      {
          table[b][i]="X";
      }
- }
 }
-    for(int i = 0; i < 10; i++) {
-       for(int j = 0; j < 10; j++) {
+  for(int i = 0; i < 10; i++) {
+       for(int j = 0; j < 10; j++){
            cout<< table[i][j]<<" ";
     }
     cout<<endl;
 }
 cout<<endl;
-std::random_device rd;
-std::mt19937 gen(rd());
-
-int min = 0;
-int max = 1;
-std::uniform_int_distribution<> dis(min, max);
-int x=dis(gen);
-if(x==0)
-int y=1;
-else if(x==1)
-int y=0;
-cout<<y<<" "<<x<<endl;
-if(x==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int a=dis(gen);
-for(int i=a; i<a+3; i++)
-{
-    table_2[a][i]="X";
-}
-}
-else if(y==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int b=dis(gen);
-for(int i=b; i<b+3; i++)
-{
-    table_2[i][b]="X";
-}
-}
-for(int k=0; k<2; k++)
-{
-if(x==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int a=dis(gen);
-for(int i=a; i<a+2; i++)
-{
-    table_2[a][i]="X";
-}
-}
-else if(y==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int b=dis(gen);
-for(int i=b; i<b+2; i++)
-{
-    table_2[i][b]="X";
-}
-}
-}
-for(int k=0; k<3; k++)
-{
-if(x==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int a=dis(gen);
-
-for(int i=a; i<a+1; i++)
-{
-    table_2[a][i]="X";
-}
-else if(y==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int b=dis(gen);
-for(int i=b; i<b+1; i++)
-{
-    table_2[i][b]="X";
-}
-}
-}
-}
-for(int k=0; k<4; k++)
-{
-if(x==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int a=dis(gen);
-    table_2[a][i]="X";
-}
-else if(y==1)
-{
-int min=1;
-int max=9;
-std::uniform_int_distribution<> dis(min, max);
-int b=dis(gen);
-    table_2[i][b]="X";
-}
-}
-
+ }
+ int t[10]={4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+ for(int k=1; k<10; k++)
+ {
+     for(int i=0; i<t[k]; i++)
+     {
+        srand(time(NULL));
+         int x=rand()%9+1;
+         int y=rand()%9+1;
+        if(table_2[x][y]==" " && table_2[x+1][y+1]==" " && table_2[x-1][y-1]==" ")
+        {
+            table_2[x][y]="X";
+        }
+ }
+ }
 }
 
 
